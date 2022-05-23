d1c335bfd0565255eae31f7b42e66cc57999a06a
shreegowri <gaganandh@gmail.com>
2022-05-23 19:15:06 +0530

    [shreegowri]to display employee present or not and calulate wage

---
+++ day9/employeewages/uc2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+ISPRESENT=1
+IS_PARTIME=2
+WAGE_PER_HR=20
+FULL_DAY_HR=8
+PART_TIME_HR=4
+randomcheck=$(($RANDOM%3))
+if [ $ISPRESENT -eq $randomcheck ]
+then
+	salary=$(($WAGE_PER_HR*$FULL_DAY_HR))
+	echo "employee present"
+ 
+
+else
+	echo "employee absent"
+fi
